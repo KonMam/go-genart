b55b880b8af4aac1f92fdd50a53c76cc632fc96e
Konstantinas Mamonas <k.mamonas@gmail.com>
2025-08-23 22:59:33 +0300

    repo scaffold

---
+++ README.md
@@ -0,0 +1,3 @@
+# Generative Art in Go
+
+This is a project to generate some images with Go and learn something new.
--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import "github.com/fogleman/gg"
 
 func main() {
-	fmt.Println("Hello World!")
+	dc := gg.NewContext(1000, 1000)
+	dc.DrawCircle(500, 500, 400)
+	dc.SetRGB(0, 0, 0)
+	dc.Fill()
+	dc.SavePNG("out.png")
 }
